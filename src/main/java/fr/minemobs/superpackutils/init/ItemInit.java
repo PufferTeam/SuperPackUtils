c2de889bf78bb92e08641edc06fefb6a089e96ff
Jerome226 <55724525+Jerome226@users.noreply.github.com>
2021-04-20 23:57:24 -0400

    Added Iron Ore Piece.

--- src/main/java/fr/minemobs/superpackutils/init/ItemInit.java
+++ src/main/java/fr/minemobs/superpackutils/init/ItemInit.java
@@ -50,6 +50,7 @@ public class ItemInit {
     public static final RegistryObject<Item> TIN_ORE_PIECE = ITEMS.register("tin_ore_piece", () -> new Item(new Item.Properties().group(Main.ModItemGroup.instance)));
     public static final RegistryObject<Item> COPPER_ORE_PIECE = ITEMS.register("copper_ore_piece", () -> new Item(new Item.Properties().group(Main.ModItemGroup.instance)));
     public static final RegistryObject<Item> GOLD_ORE_PIECE = ITEMS.register("gold_ore_piece", () -> new Item(new Item.Properties().group(Main.ModItemGroup.instance)));
+    public static final RegistryObject<Item> IRON_ORE_PIECE = ITEMS.register("iron_ore_piece", () -> new Item(new Item.Properties().group(Main.ModItemGroup.instance)));
 
     
     //Other
