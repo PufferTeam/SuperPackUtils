776254b8819a715465d27ad4ffbff45ab42ce447
Jerome226 <55724525+Jerome226@users.noreply.github.com>
2021-04-24 01:25:25 -0400

    Update BlockInit.java

--- src/main/java/fr/minemobs/superpackutils/init/BlockInit.java
+++ src/main/java/fr/minemobs/superpackutils/init/BlockInit.java
@@ -39,6 +39,8 @@ public class BlockInit {
     public static final RegistryObject<Block> DIMENSIONAL_STONE = BLOCKS.register("dimensional_stone", () -> new Block(AbstractBlock.Properties.from(Blocks.IRON_ORE)));
 
     public static final RegistryObject<Block> DYINGROCK = BLOCKS.register("dyingrock", () -> new Block(AbstractBlock.Properties.from(Blocks.STONE)));
+   
+    public static final RegistryObject<Block> INDUSTRIAL_SAND = BLOCKS.register("industrial_sand", () -> new Block(AbstractBlock.Properties.from(Blocks.SAND)));
 
     public static final RegistryObject<Block> BIG_STEEL_CASING = BLOCKS.register("big_steel_casing", () -> new Block(AbstractBlock.Properties.from(Blocks.DIAMOND_BLOCK)));
 
