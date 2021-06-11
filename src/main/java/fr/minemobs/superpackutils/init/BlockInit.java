4d5919e2628f920b33586089e79f4387a4843a3a
minemobs <issahakim94@gmail.com>
2021-06-12 00:04:47 +0200

    Added a registerBlock function

--- src/main/java/fr/minemobs/superpackutils/init/BlockInit.java
+++ src/main/java/fr/minemobs/superpackutils/init/BlockInit.java
@@ -16,6 +16,8 @@ import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.util.Locale;
+
 public class BlockInit {
 
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Main.MOD_ID);
@@ -25,45 +27,61 @@ public class BlockInit {
             () -> new TinyTorch(Block.Properties.of(Material.DECORATION).noCollission().instabreak()
                     .lightLevel((state) -> 2).sound(SoundType.WOOD), ParticleTypes.FLAME));
 
-    public static final RegistryObject<Block> REDSTONE_CLOCK = BLOCKS.register("redstone_clock",
-            RedstoneClock::new);
+    public static final RegistryObject<Block> REDSTONE_CLOCK = registerBlock("redstone_clock",
+            new RedstoneClock());
 
     public static final RegistryObject<Block> WALL_TINY_TORCH = BLOCKS.register("wall_tiny_torch",
             () -> new WallTinyTorch(Block.Properties.of(Material.DECORATION).noCollission().instabreak()
                     .lightLevel((state) -> 2).sound(SoundType.WOOD), ParticleTypes.FLAME));
 
-    public static final RegistryObject<Block> DEEPSLATE = BLOCKS.register("deepslate",
-            () -> new Block(AbstractBlock.Properties.copy(Blocks.STONE).strength(0.75F, 3.0F).harvestTool(ToolType.PICKAXE).harvestLevel(4)));
+    public static final RegistryObject<Block> DEEPSLATE = registerBlock("deepslate",
+            AbstractBlock.Properties.copy(Blocks.STONE).strength(0.75F, 3.0F).harvestTool(ToolType.PICKAXE).harvestLevel(4));
 
     //Blocks
-    public static final RegistryObject<Block> DUST_BLOCK = BLOCKS.register("dust_block", () -> new Block(AbstractBlock.Properties.copy(Blocks.SAND)));
-    public static final RegistryObject<Block> DIMENSIONAL_STONE = BLOCKS.register("dimensional_stone", () -> new Block(AbstractBlock.Properties.copy(Blocks.IRON_ORE)));
-    public static final RegistryObject<Block> DYINGROCK = BLOCKS.register("dyingrock", () -> new Block(AbstractBlock.Properties.copy(Blocks.STONE)));
-    public static final RegistryObject<Block> INDUSTRIAL_SAND = BLOCKS.register("industrial_sand", () -> new Block(AbstractBlock.Properties.copy(Blocks.SAND)));
+    public static final RegistryObject<Block> DUST_BLOCK = registerBlock("dust_block", Blocks.SAND);
+    public static final RegistryObject<Block> DIMENSIONAL_STONE = registerBlock("dimensional_stone", Blocks.IRON_ORE);
+    public static final RegistryObject<Block> DYINGROCK = registerBlock("dyingrock", Blocks.STONE);
+    public static final RegistryObject<Block> INDUSTRIAL_SAND = registerBlock("industrial_sand", Blocks.SAND);
 
     //Casing
-    public static final RegistryObject<Block> BIG_STEEL_CASING = BLOCKS.register("big_steel_casing", () -> new Block(AbstractBlock.Properties.copy(Blocks.DIAMOND_BLOCK)));
-    public static final RegistryObject<Block> INSCRIBER_CASING = BLOCKS.register("inscriber_casing", () -> new Block(AbstractBlock.Properties.copy(Blocks.DIAMOND_BLOCK)));
-    public static final RegistryObject<Block> INFUSER_CASING = BLOCKS.register("infuser_casing", () -> new Block(AbstractBlock.Properties.copy(Blocks.DIAMOND_BLOCK)));
-    public static final RegistryObject<Block> METALLURGIC_CASING = BLOCKS.register("metallurgic_casing", () -> new Block(AbstractBlock.Properties.copy(Blocks.DIAMOND_BLOCK)));
-    public static final RegistryObject<Block> IMPROVED_METALLURGIC_CASING = BLOCKS.register("improved_metallurgic_casing", () -> new Block(AbstractBlock.Properties.copy(Blocks.DIAMOND_BLOCK)));
-    public static final RegistryObject<Block> ASSEMBLER_FRAME = BLOCKS.register("assembler_frame", () -> new Block(AbstractBlock.Properties.copy(Blocks.DIAMOND_BLOCK)));
-    public static final RegistryObject<Block> SIEVE_CASING = BLOCKS.register("sieve_casing", () -> new Block(AbstractBlock.Properties.copy(Blocks.DIAMOND_BLOCK)));
+    public static final RegistryObject<Block> BIG_STEEL_CASING = registerBlock("big_steel_casing", Blocks.DIAMOND_BLOCK);
+    public static final RegistryObject<Block> INSCRIBER_CASING = registerBlock("inscriber_casing", Blocks.DIAMOND_BLOCK);
+    public static final RegistryObject<Block> INFUSER_CASING = registerBlock("infuser_casing", Blocks.DIAMOND_BLOCK);
+    public static final RegistryObject<Block> METALLURGIC_CASING = registerBlock("metallurgic_casing", Blocks.DIAMOND_BLOCK);
+    public static final RegistryObject<Block> IMPROVED_METALLURGIC_CASING = registerBlock("improved_metallurgic_casing", Blocks.DIAMOND_BLOCK);
+    public static final RegistryObject<Block> ASSEMBLER_FRAME = registerBlock("assembler_frame", Blocks.DIAMOND_BLOCK);
+    public static final RegistryObject<Block> SIEVE_CASING = registerBlock("sieve_casing", Blocks.DIAMOND_BLOCK);
 
     //Ore Blocks
-    public static final RegistryObject<Block> PLATINUM_BLOCK = BLOCKS.register("platinum_block",
-            () -> new Block(AbstractBlock.Properties.of(Material.STONE)));
+    public static final RegistryObject<Block> PLATINUM_BLOCK = registerBlock("platinum_block");
 
-    public static final RegistryObject<Block> MYTHRIL_BLOCK = BLOCKS.register("mythril_block",
-            () -> new Block(AbstractBlock.Properties.of(Material.STONE)));
+    public static final RegistryObject<Block> MYTHRIL_BLOCK = registerBlock("mythril_block");
 
     //Fluids
-    public static final RegistryObject<FlowingFluidBlock> MOLTEN_MYTHRIL_BLOCK = BlockInit.BLOCKS.register("molten_mythril_fluid",
-            () -> new FlowingFluidBlock(FluidInit.MYTHRIL_FLUID, AbstractBlock.Properties.copy(Blocks.LAVA).noCollission().strength(100.0f).noDrops().noOcclusion()));
+    public static final RegistryObject<FlowingFluidBlock> MOLTEN_MYTHRIL_BLOCK = registerBlock("molten_mythril_fluid",
+            new FlowingFluidBlock(FluidInit.MYTHRIL_FLUID, AbstractBlock.Properties.copy(Blocks.LAVA).noCollission().strength(100.0f).noDrops().noOcclusion()));
 
-    public static final RegistryObject<FlowingFluidBlock> MOLTEN_BIO_PLASTIC_BLOCK = BlockInit.BLOCKS.register("molten_bio_plastic_fluid",
-            () -> new FlowingFluidBlock(FluidInit.MOLTEN_BIO_PLASTIC_FLUID, AbstractBlock.Properties.copy(Blocks.LAVA).noCollission().strength(100.0f).noDrops().noOcclusion()));
+    public static final RegistryObject<FlowingFluidBlock> MOLTEN_BIO_PLASTIC_BLOCK = registerBlock("molten_bio_plastic_fluid",
+            new FlowingFluidBlock(FluidInit.MOLTEN_BIO_PLASTIC_FLUID, AbstractBlock.Properties.copy(Blocks.LAVA).noCollission().strength(100.0f).noDrops().noOcclusion()));
 
-    public static final RegistryObject<FlowingFluidBlock> LUBRIFIED_LPG_BLOCK = BlockInit.BLOCKS.register("lubrified_lpg_fluid",
-            () -> new FlowingFluidBlock(FluidInit.LUBRIFIED_LPG_FLUID, AbstractBlock.Properties.copy(Blocks.LAVA).noCollission().strength(100.0f).noDrops().noOcclusion()));
+    public static final RegistryObject<FlowingFluidBlock> LUBRIFIED_LPG_BLOCK = registerBlock("lubrified_lpg_fluid",
+            new FlowingFluidBlock(FluidInit.LUBRIFIED_LPG_FLUID, AbstractBlock.Properties.copy(Blocks.LAVA).noCollission().strength(100.0f).noDrops().noOcclusion()));
+
+    //Register functions
+
+    private static RegistryObject<Block> registerBlock(String name) {
+        return registerBlock(name.toLowerCase(Locale.ROOT).replaceAll(" ","_"), new Block(AbstractBlock.Properties.copy(Blocks.STONE)));
+    }
+
+    private static RegistryObject<Block> registerBlock(String name, Block.Properties properties) {
+        return registerBlock(name.toLowerCase(Locale.ROOT).replaceAll(" ","_"), new Block(properties));
+    }
+
+    private static RegistryObject<Block> registerBlock(String name, Block block) {
+        return BLOCKS.register(name.toLowerCase(Locale.ROOT).replaceAll(" ","_"), () -> block);
+    }
+
+    private static RegistryObject<FlowingFluidBlock> registerBlock(String name, FlowingFluidBlock block) {
+        return BLOCKS.register(name.toLowerCase(Locale.ROOT).replaceAll(" ","_"), () -> block);
+    }
 }
