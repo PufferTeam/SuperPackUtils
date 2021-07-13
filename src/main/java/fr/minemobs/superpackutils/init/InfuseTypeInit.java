b2beabb6a26b9faaf9e0cecb6f858837ac71d743
Jerome226 <55724525+Jerome226@users.noreply.github.com>
2021-07-12 20:05:27 -0400

    Ichor Crystal color changes and Crystallized Blood infuse type

--- src/main/java/fr/minemobs/superpackutils/init/InfuseTypeInit.java
+++ src/main/java/fr/minemobs/superpackutils/init/InfuseTypeInit.java
@@ -30,9 +30,10 @@ public class InfuseTypeInit {
 
     //Crystals
     public static final RegistryObject<InfuseType> SLIME_CRYSTAL = register("slime_crystal", 0x00FF37);
-    public static final RegistryObject<InfuseType> ICHOR_CRYSTAL = register("ichor_crystal", 0xFF4800);
+    public static final RegistryObject<InfuseType> ICHOR_CRYSTAL = register("ichor_crystal", 0xFFa600);
     public static final RegistryObject<InfuseType> SKYSLIME_CRYSTAL = register("skyslime_crystal", 0x00B3FF);
     public static final RegistryObject<InfuseType> ENDERSLIME_CRYSTAL = register("enderslime_crystal", 0xB700FF);
+    public static final RegistryObject<InfuseType> BLOOD_CRYSTAL = register("blood_crystal", 0xFC2003);
 
     /**
      * @param name Registry name of the infused type ?
